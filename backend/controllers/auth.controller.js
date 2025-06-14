6e3c5c2da4bed77a1fc06737ee7c06c6b1b4b693
Mokshith2592 <mokshithgattu@gmail.com>
2025-06-15 02:33:30 +0530

    signin done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,19 @@
 import bcrypt from 'bcrypt'
 import User from '../models/user.model.js';
+import errorHandler from '../utils/errors.js'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
-const signup = async(req ,res) => {
+dotenv.config();
+
+export const signup = async(req ,res ,next) => {
     const {username,email,password } = req.body;
 
     if(!username || !email || !password || username === "" || email === "" || password === ""){
-        return res.status(400).json({
-            message: "All fields are required"
-        })
+        // return res.status(400).json({
+        //     message: "All fields are required"
+        // })
+        return next(errorHandler(400 ,"All fields are required"))
     }
 
     const hashedPassword = bcrypt.hashSync(password ,10);
@@ -23,10 +29,43 @@ const signup = async(req ,res) => {
         res.json("Signup Successful");
     }
     catch(err) {
-        res.status(500).json({
-            message:err.message
-        })
+        // res.status(500).json({
+        //     message:err.message
+        // })
+        next(err)
     }
 }
 
-export default signup
+export const signin = async(req ,res ,next) => {
+    const {email ,password} = req.body;
+
+    if(!email || !password || email === "" || password === ""){
+        return next(errorHandler(400 ,"All fields are required"))
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+
+        if(!validUser) {
+            return next(errorHandler(404 ,"User not found"));
+        }
+
+        const validPassword = bcrypt.compareSync(password ,validUser.password)
+        if(!validPassword) {
+            return next(errorHandler(400 ,"Wrong Credentials"))
+        }
+
+        const token = jwt.sign({id: validUser._id} ,process.env.JWT_SECERET || "ijdoifdoofkwqdc")
+
+        const {password:pass, ...rest} = validUser._doc
+
+        res.status(200).cookie("access_token" ,token ,{
+            httpOnly: true,
+        }).json(rest)
+    }
+    catch(err) {
+        next(err);
+    }
+}
+
+// export default {signup ,signin};
--- backend/index.js
+++ backend/index.js
@@ -28,3 +28,14 @@ app.use('/api/auth' ,authRoutes)
 app.listen(3000 ,() => {
     console.log('App is listening on port 3000');
 }) 
+
+app.use((err ,req ,res ,next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+
+    res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    })
+})
