9667260a81e6028aa43915bdce8c9452cf76e583
Mokshith2592 <mokshithgattu@gmail.com>
2025-06-15 00:59:45 +0530

    connected to backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
 import cors from 'cors'
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv' 
+
+dotenv.config()
+
+mongoose.connect('mongodb://localhost:27017/')
+.then(() => {
+    console.log('MongoDB connected Succesfully')
+})
+.catch((error) => {
+    console.log('Failed to connect to mongoDB')
+})
+
+
 
 const app = express();
 // app.use(cors())
