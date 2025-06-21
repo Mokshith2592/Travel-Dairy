9699ba0b4e465455801a883039dfb981982413e1
Mokshith2592 <mokshithgattu@gmail.com>
2025-06-21 23:29:03 +0530

    signup ui done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,16 @@ import {BrowserRouter ,Routes ,Route} from "react-router-dom"
 import Login from "./pages/auth/Login"
 import SignUp from "./pages/auth/SignUp"
 import Home from "./pages/home/Home"
+// import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Home />}></Route>
+          </Route> */}
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/sign-up" element={<SignUp />}></Route>
---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Outlet } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+
+const PrivateRoute = () => {
+    const {currUser} = useSelector((state) => state.user)
+    if(currUser) return <Outlet />
+    return <Navigate to={"/login"} />
+}
+
+export default PrivateRoute
